721eac22900ec0061d66e63fac1d93c4c719cc4e
aquibjawed04 <aquib_j@yahoo.com>
2020-07-06 11:25:38 +0530

    Admin Dashboard UI

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -80,17 +80,4 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
