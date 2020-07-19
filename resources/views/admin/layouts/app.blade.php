d2c2fd76e89f82e8e5be4570a7a86c69bf034c0c
ANIK MANDAL <anikman98@gmail.com>
2020-07-19 11:51:15 +0530

    completed appointment

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}&nbsp;Admin</title>
+    <title>{{ config('app.name', 'BIRTH') }}&nbsp;Admin</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script> 
