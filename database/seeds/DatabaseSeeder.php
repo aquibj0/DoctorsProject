8ab1fc1c822b3d0656a7b194946791f014d3923a
ANIK MANDAL <anikman98@gmail.com>
2020-07-20 14:41:29 +0530

    added Seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,9 +5,15 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 use App\Department;
+use App\database\seeds\AdminSeeder;
+use App\database\seeds\DepartmentSeeder;
+use App\database\seeds\ServicesSeeder;
 
 class DatabaseSeeder extends Seeder
 {
+    
+
+    // private function __construct()
     /**
      * Seed the application's database.
      *
@@ -15,6 +21,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $admin = new AdminSeeder;
+        $service = new DepartmentSeeder;
+        $department = new ServicesSeeder;
+
+        $admin->run();
+        $service->run();
+        $department->run();
     }
 }
