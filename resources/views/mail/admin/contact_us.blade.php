bcc3225399b502e63179f1b8d3c840dd07d7bf7b
ANIK MANDAL <anikman98@gmail.com>
2020-08-25 10:50:41 +0530

    updated contact us mail

--- resources/views/mail/admin/contact_us.blade.php
+++ resources/views/mail/admin/contact_us.blade.php
@@ -8,9 +8,33 @@
 </head>
 <body>
     <div class="container">
+        <h3><b>New Contact Us Message</b></h3>
+    </div>
+    <hr>
+    <div class="container">
         <div class="row">
-            <div class="col-md-4"></div>
-            <div class="col-md"></div>
+            <div class="col-md-4">Name</div>
+            <div class="col-md">{{ $msg->name }}</div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">Email</div>
+            <div class="col-md">{{ $msg->email }}</div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">Phone</div>
+            <div class="col-md">{{ $msg->phone }}</div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">Address</div>
+            <div class="col-md">{{ $msg->address }}</div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">Subject</div>
+            <div class="col-md">{{ $msg->subject }}</div>
+        </div>
+        <div class="row">
+            <div class="col-md-4">Message</div>
+            <div class="col-md">{{ $msg->message }}</div>
         </div>
     </div>
 </body>
